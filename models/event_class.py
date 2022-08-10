5f1dfbfa6fafcc477ea07763550d7708dda1cc21
Grilo16 <98457056+Grilo16@users.noreply.github.com>
2022-08-10 19:15:47 +0100

    thats the right one lol

--- models/event_class.py
+++
@@ -1,9 +0,0 @@
-class Event():
-    def __init__(self, date, name, max_guests, location, description):
-        self.date = date
-        self.name = name
-        self.max_guests = max_guests
-        self.location = location
-        self.description = description
-    
-    
--- models/event_list.py
+++
@@ -1,5 +0,0 @@
-from models.event_class import Event
-
-
-event1 = Event("today", "Coding_day", "51!", "Everywhere", "Today we code, tomorrow again")
-
