509261ea4c2a0646b8c76e632bd89d5c159b1f77
Grilo16 <98457056+Grilo16@users.noreply.github.com>
2022-08-10 19:04:13 +0100

    forgot 1

---
+++ models/event_class.py
@@ -0,0 +1,9 @@
+class Event():
+    def __init__(self, date, name, max_guests, location, description):
+        self.date = date
+        self.name = name
+        self.max_guests = max_guests
+        self.location = location
+        self.description = description
+    
+    
